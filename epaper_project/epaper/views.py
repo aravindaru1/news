414f9c91346d0fea70692e5fd84c3b2d90af48d8
aravind <122278707+aravindaru1@users.noreply.github.com>
2024-06-24 14:55:07 +0530

    Update views.py

--- epaper_project/epaper/views.py
+++ epaper_project/epaper/views.py
@@ -2,15 +2,50 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import requests
+import re
+
+# URL of the page you want to fetch
+webpage_url = 'https://epaper.v6velugu.com/3883220/Telangana-Main/24-06-2024#page/1/1'
+
+# Function to extract the number from the webpage dynamically
+def extract_number_from_webpage():
+    try:
+        response = requests.get(webpage_url)
+        if response.status_code == 200:
+            html_content = response.text
+            # Regular expression pattern to find the number
+            pattern = r"https://epaper\.v6velugu\.com/r/(\d+)"
+            match = re.search(pattern, html_content)
+            if match:
+                number = match.group(1)
+                return number
+            else:
+                print("No matching number found")
+        else:
+            print(f"Failed to fetch URL: {webpage_url}. Status code: {response.status_code}")
+    except Exception as e:
+        print(f"Exception occurred while fetching URL: {webpage_url}. Error: {str(e)}")
+    
+    return None
 
 def scrape_and_display(request):
     return render(request, 'epaper/display.html')
 
 def fetch_scraped_data(request):
-    url = 'https://epaper.v6velugu.com/pagemeta/get/3883615/1-50'
-    response = requests.get(url)
-    if response.status_code == 200:
-        json_data = response.json()
-        return JsonResponse(json_data)
-    else:
-        return JsonResponse({'error': 'Failed to fetch data'}, status=response.status_code)
+    # Extract the number from the webpage
+    number = extract_number_from_webpage()
+    if not number:
+        return JsonResponse({'error': 'Failed to fetch number from webpage'}, status=500)
+    
+    # Construct the URL with the extracted number
+    url = f'https://epaper.v6velugu.com/pagemeta/get/{number}/1-50'
+    
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            json_data = response.json()
+            return JsonResponse(json_data)
+        else:
+            return JsonResponse({'error': 'Failed to fetch data'}, status=response.status_code)
+    except Exception as e:
+        return JsonResponse({'error': f'Failed to fetch data: {str(e)}'}, status=500)
