4480f4ae635e32c3c90f6bc38d70ee605cebda1b
aravind <122278707+aravindaru1@users.noreply.github.com>
2024-06-25 14:02:21 +0530

    Update urls.py

--- epaper_project/epaper/urls.py
+++ epaper_project/epaper/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import scrape_and_display, fetch_scraped_data
+from .views import fetch_news_data, index,scrape_and_display, fetch_scraped_data
 
 urlpatterns = [
+    path('fetch_news/', fetch_news_data, name='fetch_news'),
+    path('', index, name='index'),
     path('', scrape_and_display, name='scrape_and_display'),
     path('fetch_scraped_data', fetch_scraped_data, name='fetch_scraped_data'),
 ]
